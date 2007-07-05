bd74f2b21c5b6ab6a3c69f360dedfbeb2a8a963b
zybler <zybler@731ce9ac-0833-0410-ac2e-49b30577d389>
2007-07-05 15:32:32 +0000

    Added back PHP4 support.

--- trunk/pentagoo_ai.php
+++ trunk/pentagoo_ai.php
@@ -4,6 +4,38 @@ $matrix = $_GET['m'];
 $player = $_GET['p'];
 $level = $_GET['l'];
 
+// str_split function for PHP < 5
+if (!function_exists("str_split"))
+{
+    function str_split($string, $length = 1)
+    {
+        if ($length <= 0)
+        {
+            trigger_error(__FUNCTION__."(): The the length of each segment must be greater then zero:", E_USER_WARNING);
+            return false;
+        }
+        $splitted  = array();
+        $str_length = strlen($string);
+        $i = 0;
+        if ($length == 1)
+        {
+            while ($str_length--)
+                $splitted[$i] = $string[$i++];
+        }
+        else
+        {
+            $j = $i;
+            while ($str_length > 0)
+            {
+                $splitted[$j++] = substr($string, $i, $length);
+                $str_length -= $length;
+                $i += $length;
+            }
+        }
+        return $splitted;
+    }
+}
+
 // Convert matrix string to array
 $matrix_arr = str_split($matrix);
 for($m = 0; $m < 6; ++$m)
