20c1455fc52bdcb8e05e4b5843377dec258c01d7
zybler <zybler@731ce9ac-0833-0410-ac2e-49b30577d389>
2007-07-05 15:21:10 +0000

    Modified pentagoo_ai.php to allow for difficulty level selection.

--- pentagoo_ai.php
+++ pentagoo_ai.php
@@ -2,49 +2,27 @@
 // Get parameters
 $matrix = $_GET['m'];
 $player = $_GET['p'];
-
-// str_split function for PHP < 5
-if (!function_exists("str_split")) {
-    function str_split($string, $length = 1) {
-        if ($length <= 0) {
-            trigger_error(__FUNCTION__."(): The the length of each segment must be greater then zero:", E_USER_WARNING);
-            return false;
-        }
-        $splitted  = array();
-        $str_length = strlen($string);
-        $i = 0;
-        if ($length == 1) {
-            while ($str_length--) {
-                $splitted[$i] = $string[$i++];
-            }
-        } else {
-            $j = $i;
-            while ($str_length > 0) {
-                $splitted[$j++] = substr($string, $i, $length);
-                $str_length -= $length;
-                $i += $length;
-            }
-        }
-        return $splitted;
-    }
-}
+$level = $_GET['l'];
 
 // Convert matrix string to array
 $matrix_arr = str_split($matrix);
-for( $m=0 ; $m<6 ; $m++)
-	for( $n=0 ; $n<6 ; $n++)
+for($m = 0; $m < 6; ++$m)
+	for($n = 0; $n < 6 ; ++$n)
 		$matrix_array[$m][$n] = array_shift($matrix_arr);
 
-// Randomizer AI
-mt_srand();
-do{
-	$x = mt_rand(0,5);
-	$y = mt_rand(0,5);
+switch($level)
+{
+	case '0':
+		include_once('ai_0.php'); // Easiest AI - Random
+	case '1':
+		include_once('ai_1.php'); // Easy
+	case '2':
+		include_once('ai_2.php'); // Medium
+	case '3':
+		include_once('ai_3.php'); // Hard
+	case '4':
+		include_once('ai_4.php'); // God-like
 }
-while($matrix_array[$y][$x] != 0);
-$t = mt_rand(1,4);
-$d = mt_rand(0,1) ? 'l' : 'r';
 
-// OUTPUT!
-echo $x.$y.$t.$d;
+echo $x.$y.$t.$d; // OUTPUT!
 ?>
