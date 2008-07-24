7b759fd5c0c79c8fb9e5e34cce4b08860364cfc9
cheeaun <cheeaun@731ce9ac-0833-0410-ac2e-49b30577d389>
2008-07-24 17:38:18 +0000

    fix checked items

--- scripts/pentagoo.js
+++ scripts/pentagoo.js
@@ -73,8 +73,6 @@ function initialize(){
 	$('player-1-label').addClass('current');
 	$('player-2-label').removeClass('current');
 	player_type = 1;
-	$('p1-h-l').checked = true;
-	$('p2-h-l').checked = true;
 
 	// Styles for rotation buttons
 	$$('.rotation-buttons').setStyle('opacity', 0);
